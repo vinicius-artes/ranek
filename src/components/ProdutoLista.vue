96fb4e0a8d1891b08448a4bbaac771dc189600cc
Vinicius <93890843+vinicius-artes@users.noreply.github.com>
2024-03-08 23:58:32 -0300

    Paginação

--- src/components/ProdutoLista.vue
+++ src/components/ProdutoLista.vue
@@ -1,22 +1,37 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
+    {{ produtosTotal }}
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div v-for="(produto, index) in produtos" :key="index" class="produto">
+      <router-link to="">
       <img v-if="produto.fotos" :src="produto.fotos[0]" :alt="produto.fotos[0].titulo">
       <p class="preco">{{ produto.preco }}</p>
       <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="preco">{{ produto.descricao }}</p>
+      <p>{{ produto.descricao }}</p>
+      </router-link>
+      </div>
+      <produtosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina" />
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
 
 <script>
+import ProdutosPaginar from "@/components/ProdutosPaginar.vue";
 import { api } from "@/services.js";
-import { serialize } from "@/helpers.js"
+import { serialize } from "@/helpers.js";
 export default {
+  name: "ProdutosLista",
+  components: {
+    ProdutosPaginar
+  },
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 9,
+      produtosPorPagina: 3,
+      produtosTotal: 0
     }
   },
   computed: {
@@ -25,11 +40,11 @@ export default {
 
       return `/produto?_limit=${this.produtosPorPagina}${query}`;
     }
-    
   },
   methods: {
     getProdutos() {
       api.get(this.url).then(response => {
+        this.produtosTotal = Number(response.headers["x-total-count"]);
         this.produtos = response.data;
       });
     }
@@ -45,6 +60,42 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin: 30px;
+}
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+.titulo {
+  margin-bottom: 10px;
+}
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+.sem-resultados {
+  text-align: center;
+}
 </style>
