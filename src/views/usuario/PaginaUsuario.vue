52c3fdb8078f3f0e5ac567db496401c4f41d4f3a
Vinicius <93890843+vinicius-artes@users.noreply.github.com>
2024-03-14 23:58:00 -0300

    adicionando produtos

--- src/views/usuario/PaginaUsuario.vue
+++ src/views/usuario/PaginaUsuario.vue
@@ -6,13 +6,12 @@
         <li><router-link :to="{ name: 'compras' }">Compras</router-link></li>
         <li><router-link :to="{ name: 'vendas' }">Vendas</router-link></li>
         <li><router-link :to="{ name: 'usuario-editar' }">Editar Usuário</router-link></li>
-        <li><button @click.prevent="deslogar">Deslogar</button></li>
+        <li><button @click="deslogar">Deslogar</button></li>
       </ul>
     </nav>
     <transition mode="out-in">
       <router-view></router-view>  
     </transition>
-    <div>Pagina do usuário: {{  $store.state.usuario }}</div>
   </section>
   
 </template>
@@ -30,6 +29,39 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  gap: 30px;
+  padding: 20px;
+}
+@media (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+  }
+}
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+.sidenav a.router-link-exact-active, .sidenav a:hover, .sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: Helvetica, Arial,  sans-serif;
+  cursor: pointer;
+}
 </style>
--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,13 +1,60 @@
 <template>
-  <div>Lista de produtos</div>
+  <section>
+    <h2>Adicionar Produto</h2>
+    <ProdutoAdicionar />
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{ produto.descricao }}</p>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {
+import ProdutoAdicionar from '@/components/ProdutoAdicionar.vue';
+import ProdutoItem from '@/components/ProdutoItem.vue';
+import { mapState, mapActions } from "vuex";
 
-}
+export default {
+  name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionar,
+    ProdutoItem
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    } 
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all .3s;
+}
 
 </style>
